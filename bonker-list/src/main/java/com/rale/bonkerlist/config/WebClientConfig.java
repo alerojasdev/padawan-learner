9c893ae8f64302dd1266e01b16eb9a7509405ac8
Alejandro Rojas <alerojasdev@gmail.com>
2023-07-24 14:50:49 -0400

    increase limit buffering of data in-memory

--- bonker-list/src/main/java/com/rale/bonkerlist/config/WebClientConfig.java
+++ bonker-list/src/main/java/com/rale/bonkerlist/config/WebClientConfig.java
@@ -1,6 +1,4 @@
 package com.rale.bonkerlist.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
@@ -12,48 +10,40 @@ import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepo
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
-
 @Configuration
 public class WebClientConfig {
-
-    private final ClientRegistrationRepository clientRegistrationRepository;
-    private final OAuth2AuthorizedClientRepository authorizedClientRepository;
-
-    @Autowired
-    public WebClientConfig(ClientRegistrationRepository clientRegistrationRepository,
-                           OAuth2AuthorizedClientRepository authorizedClientRepository) {
-        this.clientRegistrationRepository = clientRegistrationRepository;
-        this.authorizedClientRepository = authorizedClientRepository;
-    }
-
     @Bean
-    public WebClient webClient() {
+    public OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository) {
+        OAuth2AuthorizedClientProvider authorizedClientProvider =
+                OAuth2AuthorizedClientProviderBuilder.builder()
+                        .authorizationCode()
+                        .refreshToken()
+                        .build();
+        System.out.println("I AM OAUTH2 AUTHORIZED CLIENT MANAGER ");
+        DefaultOAuth2AuthorizedClientManager authorizedClientManager =
+                new DefaultOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository, authorizedClientRepository);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+        return authorizedClientManager;
+    }
+    @Bean
+    public WebClient webClient(OAuth2AuthorizedClientManager authorizedClientManager) {
+
         final int size = 16 * 1024 * 1024;
         final ExchangeStrategies strategies = ExchangeStrategies.builder()
                 .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
                 .build();
 
         ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
-                new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager());
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
         oauth2Client.setDefaultClientRegistrationId("spotify");
-
+        System.out.println("I AM WEBCLIENT");
         return WebClient.builder()
+                .exchangeStrategies(strategies)
                 .baseUrl("https://api.spotify.com/v1")
                 .apply(oauth2Client.oauth2Configuration())
-                .exchangeStrategies(strategies)
                 .build();
-    }
-    private OAuth2AuthorizedClientManager authorizedClientManager() {
-        OAuth2AuthorizedClientProvider authorizedClientProvider =
-                OAuth2AuthorizedClientProviderBuilder.builder()
-                        .authorizationCode()
-                        .refreshToken()
-                        .build();
-
-        DefaultOAuth2AuthorizedClientManager authorizedClientManager =
-                new DefaultOAuth2AuthorizedClientManager(
-                        clientRegistrationRepository, authorizedClientRepository);
-        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
-        return authorizedClientManager;
     }
 }
